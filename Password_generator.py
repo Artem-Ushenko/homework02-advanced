1492529a6251ecb3369ba4f2670609046ac74f81
Artem_Ushenko <kingart030192kingart@gmail.com>
2023-07-08 18:21:45 +0300

    New version of Password_generator.py (added all conditions from task)

--- Password_generator.py
+++ Password_generator.py
@@ -1,11 +1,22 @@
+import string
 import random
 
-length = int(input("""Welcome to the Linux User Password Generator!
+def generator_password(length_password):
+    all_symbols = string.ascii_letters + string.digits + string.punctuation
 
-Please enter the desired password length: """))
+    condition = [random.choice(string.ascii_uppercase), random.choice(string.ascii_lowercase),
+               random.choice(string.digits), random.choice(string.punctuation)]
 
-password_characters = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 0, 1, 2, 3, 4, 5, 6, 7, 8, 9,'!', '"', '#', '$', '%', '&', "'", '(', ')', '*', '+', ',', '-', '.', '/', ':', ';', '<', '=', '>', '?', '@', '[', '\\', ']', '^', '_', '`', '{', '|', '}', '~']
+    password = random.sample(condition, len(condition)) + random.sample(all_symbols, length_password - len(condition))
 
-for i in range(length):
-    character = random.choice(password_characters)
-    print(character, end="")
+    random.shuffle(password)
+
+    return ''.join(password)
+
+length_password = int(input("length_password = "))
+if length_password < 4:
+    print("Please enter length a greater than 4 symbols to make the password stronger !!!")
+else:
+    print("Your password: ", generator_password(length_password))
+
+
