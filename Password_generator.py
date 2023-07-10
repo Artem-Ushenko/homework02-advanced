ed9e4bbba89db3a9cd4adff37d60c5521e187dea
Artem_Ushenko <kingart030192kingart@gmail.com>
2023-07-10 10:57:01 +0300

    Was fixed some errors

--- Password_generator.py
+++ Password_generator.py
@@ -5,7 +5,7 @@ def generator_password(length_password):
     all_symbols = string.ascii_letters + string.digits + string.punctuation
 
     condition = [random.choice(string.ascii_uppercase), random.choice(string.ascii_lowercase),
-               random.choice(string.digits), random.choice(string.punctuation)]
+                 random.choice(string.digits), random.choice(string.punctuation)]
 
     password = random.sample(condition, len(condition)) + random.sample(all_symbols, length_password - len(condition))
 
@@ -13,10 +13,15 @@ def generator_password(length_password):
 
     return ''.join(password)
 
-length_password = int(input("length_password = "))
-if length_password < 4:
-    print("Please enter length a greater than 4 symbols to make the password stronger !!!")
-else:
-    print("Your password: ", generator_password(length_password))
 
+try:
+    print("Welcome to the Linux User Password Generator!")
+
+    length_password = int(input("Please enter the desired password length: "))
+    if length_password < 4:
+        print("Please enter length a greater than 4 symbols to make the password stronger !!!")
+    else:
+        print("Your password: ", generator_password(length_password))
+except ValueError:
+    print("Incorrect input !!! Please enter true length for password !!!")
 
